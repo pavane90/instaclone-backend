bc1d1c7a03ef74bfc2c0cc0c85b0825339e319a4
byungmanleetokyo <tokyo@terraworld.me>
2022-04-03 13:34:01 +0900

    protecting resolvers

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,45 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import client from "../../client";
+import { protectResolver } from "../users.utils";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: protectResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { loggedInUser, protectResolver }
+      ) => {
+        //패스워드 수정시 암호화
+        let uglyPassword = null;
+        if (newPassword) {
+          uglyPassword = await bcrypt.hash(newPassword, 10);
+        }
+
+        const updatedUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassword && { password: uglyPassword }),
+          },
+        });
+        if (updatedUser.id) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "could not edit profile.",
+          };
+        }
+      }
+    ),
   },
 };
